31be40d2bf8d3ae77706b2096b2548c66a1c9f23
Shanya-Sahu <shanyasahu11@gmail.com>
2024-03-16 12:46:36 +0530

    Nearby Attraction Content Updated.

--- src/app/nearbyattractions/page.jsx
+++ src/app/nearbyattractions/page.jsx
@@ -17,15 +17,15 @@ export default function NearbyAttractionsPage() {
           Near By Attractions
         </h2>
 
-    {/* Konark Sun Temple start */}
+        {/* Konark Sun Temple start */}
         <div className="m-5 flex justify-center items-center flex-wrap flex-col md:flex-row sm:mx-[20px] md:mx-[100px] lg:mx-[150px] xl:mx-[200px] bg-[rgba(0,0,0,0.3)] rounded-xl p-10">
           <div className="w-full md:w-1/4">
             <Image
               src="/assets/konark.png"
               alt="location_img"
               width={1000}
-              height={100}
-              className="w-full h-[100]"
+              height={50}
+              className="w-full h-[50]"
             />
           </div>
           <div className="w-full md:w-9/12">
@@ -41,27 +41,25 @@ export default function NearbyAttractionsPage() {
             </div>
 
             <p className="text-center md:text-justify mt-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              veritatis asperiores deserunt corrupti laudantium blanditiis aut
-              eaque eos molestiae numquam saepe, odio illo itaque repudiandae
-              nobis amet quaerat quasi! Tenetur neque fugiat sapiente quos
-              impedit commodi veritatis, dolorum, odio ea aut ratione debitis
-              excepturi voluptates laboriosam? Aspernatur maiores error beatae.
+              Located in Odisha, India, the Konark Sun Temple is a UNESCO World
+              Heritage Site known for its exquisite architecture dedicated to
+              the sun god, Surya. The temple's intricate stone carvings
+              depicting various mythological scenes and celestial beings attract
+              visitors from around the world.
             </p>
           </div>
         </div>
-    {/* Konark Sun Temple start */}
+        {/* Konark Sun Temple start */}
 
-
- {/* Chandipur start */}
- <div className="m-5 flex justify-center items-center flex-wrap flex-col md:flex-row sm:mx-[20px] md:mx-[100px] lg:mx-[150px] xl:mx-[200px] bg-[rgba(0,0,0,0.3)] rounded-xl p-10">
+        {/* Chandipur start */}
+        <div className="m-5 flex justify-center items-center flex-wrap flex-col md:flex-row sm:mx-[20px] md:mx-[100px] lg:mx-[150px] xl:mx-[200px] bg-[rgba(0,0,0,0.3)] rounded-xl p-10">
           <div className="w-full md:w-1/4">
             <Image
               src="/assets/chandipur.png"
               alt="location_img"
               width={1000}
-              height={100}
-              className="w-full h-[100]"
+              height={50}
+              className="w-full h-[50]"
             />
           </div>
           <div className="w-full md:w-9/12">
@@ -72,32 +70,30 @@ export default function NearbyAttractionsPage() {
             <div className="flex justify-center items-center flex-col md:justify-start md:items-start md:flex-row">
               <PiMapPinLineLight className="hidden text-xl mr-2 md:block" />
               <p className="uppercase text-center md:text-left">
-              Balasore District, Odisha
+                Balasore District, Odisha
               </p>
             </div>
 
             <p className="text-center md:text-justify mt-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              veritatis asperiores deserunt corrupti laudantium blanditiis aut
-              eaque eos molestiae numquam saepe, odio illo itaque repudiandae
-              nobis amet quaerat quasi! Tenetur neque fugiat sapiente quos
-              impedit commodi veritatis, dolorum, odio ea aut ratione debitis
-              excepturi voluptates laboriosam? Aspernatur maiores error beatae.
+              A unique beach town in Odisha, Chandipur is famous for its
+              disappearing sea phenomenon. During low tide, the sea recedes up
+              to 5 kilometers, exposing the seabed, and then returns during high
+              tide. This natural occurrence is a major attraction for tourists
+              visiting the region.
             </p>
           </div>
         </div>
-    {/* Chandipur start */}
+        {/* Chandipur start */}
 
-
- {/* Puri start */}
- <div className="m-5 flex justify-center items-center flex-wrap flex-col md:flex-row sm:mx-[20px] md:mx-[100px] lg:mx-[150px] xl:mx-[200px] bg-[rgba(0,0,0,0.3)] rounded-xl p-10">
+        {/* Puri start */}
+        <div className="m-5 flex justify-center items-center flex-wrap flex-col md:flex-row sm:mx-[20px] md:mx-[100px] lg:mx-[150px] xl:mx-[200px] bg-[rgba(0,0,0,0.3)] rounded-xl p-10">
           <div className="w-full md:w-1/4">
             <Image
               src="/assets/puri.png"
               alt="location_img"
               width={1000}
-              height={100}
-              className="w-full h-[100]"
+              height={50}
+              className="w-full h-[50]"
             />
           </div>
           <div className="w-full md:w-9/12">
@@ -107,33 +103,29 @@ export default function NearbyAttractionsPage() {
 
             <div className="flex justify-center items-center flex-col md:justify-start md:items-start md:flex-row">
               <PiMapPinLineLight className="hidden text-xl mr-2 md:block" />
-              <p className="uppercase text-center md:text-left">
-              Odisha
-              </p>
+              <p className="uppercase text-center md:text-left">Odisha</p>
             </div>
 
             <p className="text-center md:text-justify mt-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              veritatis asperiores deserunt corrupti laudantium blanditiis aut
-              eaque eos molestiae numquam saepe, odio illo itaque repudiandae
-              nobis amet quaerat quasi! Tenetur neque fugiat sapiente quos
-              impedit commodi veritatis, dolorum, odio ea aut ratione debitis
-              excepturi voluptates laboriosam? Aspernatur maiores error beatae.
+              Known for its beautiful beaches and the famous Jagannath Temple,
+              Puri is a significant pilgrimage destination in India. The
+              Jagannath Temple hosts the Rath Yatra festival, during which
+              massive chariots carrying deities are pulled by devotees, drawing
+              millions of visitors annually.
             </p>
           </div>
         </div>
-    {/* Puri start */}
+        {/* Puri start */}
 
-
- {/* Nandankanan start */}
- <div className="m-5 flex justify-center items-center flex-wrap flex-col md:flex-row sm:mx-[20px] md:mx-[100px] lg:mx-[150px] xl:mx-[200px] bg-[rgba(0,0,0,0.3)] rounded-xl p-10">
+        {/* Nandankanan start */}
+        <div className="m-5 flex justify-center items-center flex-wrap flex-col md:flex-row sm:mx-[20px] md:mx-[100px] lg:mx-[150px] xl:mx-[200px] bg-[rgba(0,0,0,0.3)] rounded-xl p-10">
           <div className="w-full md:w-1/4">
             <Image
               src="/assets/nandankanan.png"
               alt="location_img"
               width={1000}
-              height={100}
-              className="w-full h-[100]"
+              height={50}
+              className="w-full h-[50]"
             />
           </div>
           <div className="w-full md:w-9/12">
@@ -144,100 +136,90 @@ export default function NearbyAttractionsPage() {
             <div className="flex justify-center items-center flex-col md:justify-start md:items-start md:flex-row">
               <PiMapPinLineLight className="hidden text-xl mr-2 md:block" />
               <p className="uppercase text-center md:text-left">
-              Barang, Bhubaneswar, Odisha
+                Barang, Bhubaneswar, Odisha
               </p>
             </div>
 
             <p className="text-center md:text-justify mt-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              veritatis asperiores deserunt corrupti laudantium blanditiis aut
-              eaque eos molestiae numquam saepe, odio illo itaque repudiandae
-              nobis amet quaerat quasi! Tenetur neque fugiat sapiente quos
-              impedit commodi veritatis, dolorum, odio ea aut ratione debitis
-              excepturi voluptates laboriosam? Aspernatur maiores error beatae.
+              Nandankanan Zoological Park, located near Bhubaneswar, is renowned
+              for its conservation efforts and housing a diverse range of
+              wildlife, including white tigers and Asiatic lions. It is also a
+              botanical garden with a picturesque lake, making it a popular spot
+              for nature enthusiasts.
             </p>
           </div>
         </div>
-    {/* Nandankanan start */}
+        {/* Nandankanan start */}
 
-
- {/* Chilka lake
+        {/* Chilka lake
  start */}
- <div className="m-5 flex justify-center items-center flex-wrap flex-col md:flex-row sm:mx-[20px] md:mx-[100px] lg:mx-[150px] xl:mx-[200px] bg-[rgba(0,0,0,0.3)] rounded-xl p-10">
+        <div className="m-5 flex justify-center items-center flex-wrap flex-col md:flex-row sm:mx-[20px] md:mx-[100px] lg:mx-[150px] xl:mx-[200px] bg-[rgba(0,0,0,0.3)] rounded-xl p-10">
           <div className="w-full md:w-1/4">
             <Image
               src="/assets/chilkalake.png"
               alt="location_img"
               width={1000}
-              height={100}
-              className="w-full h-[100]"
+              height={50}
+              className="w-full h-[50]"
             />
           </div>
           <div className="w-full md:w-9/12">
             <h2 className="mt-5 md:mt-0 text-2xl md:text-3xl font-bold md:leading-[45px] text-center md:text-left">
-              Chilka lake
-
+            Chilika Lake
             </h2>
 
             <div className="flex justify-center items-center flex-col md:justify-start md:items-start md:flex-row">
               <PiMapPinLineLight className="hidden text-xl mr-2 md:block" />
-              <p className="uppercase text-center md:text-left">
-                Odisha
-              </p>
+              <p className="uppercase text-center md:text-left">Odisha</p>
             </div>
 
             <p className="text-center md:text-justify mt-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              veritatis asperiores deserunt corrupti laudantium blanditiis aut
-              eaque eos molestiae numquam saepe, odio illo itaque repudiandae
-              nobis amet quaerat quasi! Tenetur neque fugiat sapiente quos
-              impedit commodi veritatis, dolorum, odio ea aut ratione debitis
-              excepturi voluptates laboriosam? Aspernatur maiores error beatae.
+              Asia's largest brackish water lagoon, Chilika Lake is a haven for
+              migratory birds, especially during the winter months. Birdwatchers
+              flock to this site to catch glimpses of rare avian species like
+              flamingos, herons, and pelicans amidst the tranquil waters and
+              lush greenery.
             </p>
           </div>
         </div>
-    {/* Chilka lake
+        {/* Chilka lake
  start */}
 
-
- {/* Udayagiri and
+        {/* Udayagiri and
 Khandagiri Caves start */}
- <div className="m-5 flex justify-center items-center flex-wrap flex-col md:flex-row sm:mx-[20px] md:mx-[100px] lg:mx-[150px] xl:mx-[200px] bg-[rgba(0,0,0,0.3)] rounded-xl p-10">
+        <div className="m-5 flex justify-center items-center flex-wrap flex-col md:flex-row sm:mx-[20px] md:mx-[100px] lg:mx-[150px] xl:mx-[200px] bg-[rgba(0,0,0,0.3)] rounded-xl p-10">
           <div className="w-full md:w-1/4">
             <Image
               src="/assets/udayagiri.png"
               alt="location_img"
               width={1000}
-              height={100}
-              className="w-full h-[100]"
+              height={50}
+              className="w-full h-[50]"
             />
           </div>
           <div className="w-full md:w-9/12">
             <h2 className="mt-5 md:mt-0 text-2xl md:text-3xl font-bold md:leading-[45px] text-center md:text-left">
-              Udayagiri and
-Khandagiri Caves
+              Udayagiri and Khandagiri Caves
             </h2>
 
             <div className="flex justify-center items-center flex-col md:justify-start md:items-start md:flex-row">
               <PiMapPinLineLight className="hidden text-xl mr-2 md:block" />
               <p className="uppercase text-center md:text-left">
-              Historical landmark in Bhubaneswar, Odisha
+                Historical landmark in Bhubaneswar, Odisha
               </p>
             </div>
 
             <p className="text-center md:text-justify mt-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-              veritatis asperiores deserunt corrupti laudantium blanditiis aut
-              eaque eos molestiae numquam saepe, odio illo itaque repudiandae
-              nobis amet quaerat quasi! Tenetur neque fugiat sapiente quos
-              impedit commodi veritatis, dolorum, odio ea aut ratione debitis
-              excepturi voluptates laboriosam? Aspernatur maiores error beatae.
+              These ancient Jain rock-cut caves near Bhubaneswar showcase
+              intricate carvings, sculptures, and inscriptions dating back to
+              the 2nd century BCE. The caves provide a glimpse into Jain and
+              Buddhist art and architecture, offering a fascinating historical
+              and cultural experience for visitors.
             </p>
           </div>
         </div>
-    {/* Udayagiri and
+        {/* Udayagiri and
 Khandagiri Caves start */}
-
       </section>
     </section>
   );
--- src/components/event/index.jsx
+++ src/components/event/index.jsx
@@ -12,12 +12,12 @@ const Popup = ({ isOpen, onClose, content }) => {
       <div className="fixed inset-0 backdrop-blur-sm"></div>
       
       <div className="mt-16 md:mt-0 relative z-50 bg-[var(--primary-color)] shadow-lg w-[90%] md:w-[50%] rounded-md p-10">
-        <button onClick={onClose} className="absolute top-0 right-0 p-3">
+        <button onClick={onClose} className="absolute top-2 right-0 p-3">
           <span className="sr-only">Close</span>
           <IoClose className="text-white text-3xl" />
         </button>
         <div className='flex justify-between items-center w-full flex-col-reverse md:flex-row '>
-        <div className='w-[95%] md:w-[70%] md:pr-5'>
+        <div className='w-full'>
         <div className="w-full flex justify-start items-center flex-wrap">
             <div className="w-[35%] md:w-fit text-right border-dotted border-r-2 pr-3 border-white mr-3">
               <h2 className="font-bold text-3xl mb-1 md:mb-0 md:text-5xl md:leading-[40px]">{content.date}</h2>
@@ -27,12 +27,12 @@ const Popup = ({ isOpen, onClose, content }) => {
               <h2 className="text-lg md:text-3xl font-extrabold md:leading-10">{content.title}</h2>
               <p className="uppercase text-xs md:text-base">{content.subtitle}</p>
             </div>
-            <p className='mt-5 text-sm md:text-base text-center md:text-left'>{content.description}</p>
+            <p className='mt-5 text-sm md:text-base text-center md:text-justify'>{content.description}</p>
           </div>
         </div>
-        <div className='w-[95%] md:w-[30%] my-5 md:my-0'>
+        {/* <div className='w-[95%] md:w-[30%] my-5 md:my-0'>
         <Image src={content.src} alt='event_img' width={500} height={900} className='w-full h-[100px] md:h-[200px] object-cover rounded-lg'/>
-        </div>
+        </div> */}
        
         </div>
 
@@ -61,49 +61,42 @@ const EventSection = () => {
       title: "TARANG",
       subtitle: "(Talent Show for Persons with Disabilities)",
       description: "TARANG is one of the most important events of KAIRAAN that aims to ignite inclusivity and celebrate the artistic spirit of specially-abled individuals. The event explores an array of fun activities, ranging from games to singing and dancing competitions, witnessing exceptional performances. It encourages individuals to go the extra mile by overcoming their reservations and showcasing their true selves. Last year, TARANG observed active participation from schools in and around Bhubaneswar and Cuttack. As an event, it provides an amazing opportunity for its participants to strengthen their interaction with society.",
-      src:"/assets/b7.jpg",
     },
     {
       date: "21",
       title: "Project Runaway",
       subtitle: "(Fashion Show)",
       description: `Take a Deep Dive into your closet and dress to impress for Project Runway- where glamour takes centre stage once again! Join us for a night of unparalleled style and runway magic as we showcase the dreamiest looks and trendsetting designs. Be part of the ultimate chic affair and prepare to make every step a statement at Project Runway! Don your heart out on your sleeve and dazzle everyone around with your chicest outfit!`,
-      src:"/assets/b7.jpg",
     },
     {
       date: "22",
       title: "Camp Rock",
       subtitle: "(Battle of Bands)",
       description: "Get ready to unleash the thunder at KAIRAAN’s electrifying 'Battle of the Bands' – Camp Rock! This adrenaline pumping event is not just a platform for seasoned artists but an open invitation for all music enthusiasts. Whether you’re into classic rock, indie vibes, or the latest pop anthems, there’s a spot for every musical tribe. Gather your bandmates, fine-tune your instruments, and hit us with your hottest track! Let the music roar!",
-      src:"/assets/b7.jpg",
     },
     {
       date: "22",
       title: "RANGMANCH",
       subtitle: "(Theatre competition)",
       description: "The vibrant cultural tapestry of KAIRAAN, unfurls its first chapter of Rangmanch, a captivating exploration of the world of theatre arts. This event promises to transport you to a realm of storytelling, expression, and raw human emotions. From thought-provoking skits and monologues to captivating full-length plays, Rangmanch aims to showcase the talent of budding thespians from NLUO and beyond.",
-      src:"/assets/b7.jpg",
     },
     {
       date: "22",
       title: "Pitch Perfect",
       subtitle: "(Solo Singing Competition)",
       description: "As the spotlight illuminates the stage, each performer steps into the limelight, ready to mesmerise the audience with their melodious voice. From soothing tunes to upbeat pop anthems, the solo singing performances span across various genres reflecting the diverse musical tastes of the contestants. With every note sung, be the protagonist of your own musical universe, and create an emotional and enchanting atmosphere that captivates everyone.",
-      src:"/assets/b7.jpg",
     },
     {
       date: "22",
       title: "Step Up",
       subtitle: "(Solo Dance Competition)",
       description: `Set the stage on fire with "Step Up" by bringing your A-game to the floor. In “Step up,” with a mix of styles from hip-hop to contemporary, each performer tells their story through movement. The energy is contagious as the audience cheers on the dancers, swept up in the rhythm and passion on display. Judges watch closely, evaluating technique, creativity, and stage presence. The atmosphere crackles with excitement, making "Step Up" a highlight of the fest where talent shines the brightest under the spotlight.`,
-      src:"/assets/b7.jpg",
     },
     {
       date: "23",
       title: "High Strung",
       subtitle: "(Group Dance Competition)",
       description: `"High Strung" takes centre stage at our college cultural fest with its exhilarating group dance competition. Here, teams of dancers come together to showcase their collective talent and creativity, setting the floor ablaze with their synchronised moves and infectious energy. At KAIRAAN, we have performances ranging from contemporary fusions to traditional cultural expressions, each telling a captivating story, drawing the audience into a whirlwind of rhythm and motion. With cheers echoing and spirits soaring, "High Strung" transforms the fest into a vibrant celebration of unity and artistry that leaves a lasting impression on all who attend.`,
-      src:"/assets/b7.jpg",
     }
   ];
 
